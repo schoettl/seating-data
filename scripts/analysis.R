315f63b68d566cce05fab9e8fefdefb0802a3d6f
Jakob Schöttl <jschoett@gmail.com>
2016-08-17 13:27:48 +0200

    Print tables with column names

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,10 +1,41 @@
+## ---- init ----
 library(ggplot2)
 library(dplyr)
+library(xtable)
+library(knitr)
 
-surveyData   = read.csv('../data/SURVEY.csv')
-personData   = read.csv('../data/PERSON.csv')
-logEventData = read.csv('../data/LOG_EVENT.csv')
+dataPath = '../../../seating-data/data'
+
+getCsvFileName = function(baseName) {
+    return(paste0(dataPath, '/', baseName, '.csv'))
+}
+
+readCsvFile = function(baseName) {
+    return(read.csv(getCsvFileName(baseName)))
+}
+
+surveyData   = readCsvFile('SURVEY')
+personData   = readCsvFile('PERSON')
+logEventData = readCsvFile('LOG_EVENT')
 
 # with(personData, ...) has no effect to the outside
 personData$M_GROUP[personData$M_GROUP == 0] = NA
 logEventData$EXTRA_STRING[logEventData$EXTRA_STRING == ''] = NA
+
+
+
+
+makeTableWithColumns = function(dataframe) {
+    columnTable = data.frame(colnames(dataframe))
+    colnames(columnTable) = c('field name')
+    xtable(columnTable)
+}
+
+## ---- survey-table ----
+makeTableWithColumns(surveyData)
+
+## ---- person-table ----
+makeTableWithColumns(personData)
+
+## ---- logevent-table ----
+makeTableWithColumns(logEventData)
