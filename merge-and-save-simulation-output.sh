bac8b2ef0fe95d7b976a5afde06ee92c42f28689
Jakob Schöttl <jschoett@gmail.com>
2016-09-30 18:24:30 +0200

    Update helper script

--- merge-and-save-simulation-output.sh
+++ merge-and-save-simulation-output.sh
@@ -3,9 +3,13 @@
 
 printUsage() {
     cat <<EOF
-usage: $PROGNAME <output-directory>
+usage: $PROGNAME [ <output-directory> ]
+  If output directory is ommitted, the latest output in DEFAULT_OUTPUT_DIR is used.
+  If output directory is not found, the DEFAULT_OUTPUT_DIR is searched for it.
 EOF
 }
+
+readonly DEFAULT_OUTPUT_DIR=~/vadere/PersSchoettl/vadere_project/output
 
 readonly PROGNAME=$(basename "$0")
 readonly PROGDIR=$(dirname "$(readlink -m "$0")")
@@ -16,8 +20,24 @@ exitWithError() {
     exit 1
 }
 
+printLatestOutputDir() {
+    find "$DEFAULT_OUTPUT_DIR" -maxdepth 1 -mindepth 1 -type d \
+        | sed -r 's/.*_([^_]+_[^_]+)/\1 \0/' \
+        | sort \
+        | tail -1 \
+        | sed 's/[^ ]* //'
+}
+
 main() {
-    declare outputDir=$1
+    declare outputDir
+    if (( $# == 0 )); then
+        outputDir=$(printLatestOutputDir)
+    elif (( $# == 1 )); then
+        outputDir=$1
+        [[ -d $outputDir ]] || outputDir=$DEFAULT_OUTPUT_DIR/$outputDir
+    else
+        exitWithError "$(printUsage)"
+    fi
     [[ -n $outputDir ]] \
         || exitWithError "error: no simulation output directory specified."
 
